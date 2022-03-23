88e9a649eb024d6656c239d31021aa83c02e7747
Minh <quangminh8699@gmail.com>
2022-03-23 16:42:49 +0700

    update slider compelete

--- src/container/home/Components/HomeBrands.jsx
+++ src/container/home/Components/HomeBrands.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../Assets/Css/HomeBrands.css'
-import Slider from 'react-slick'
+import Slider from "react-slick";
 import Asus from '../Assets/Images/asus.png'
 import Hp from '../Assets/Images/hp.png'
 import Akko from '../Assets/Images/akko.png'
@@ -13,91 +13,154 @@ import Dareu from '../Assets/Images/dareu.png'
 import Corsair from '../Assets/Images/corsair.png'
 import Gs from '../Assets/Images/gskill.jpg'
 import Tnc from '../Assets/Images/tnc.png'
-import slider from 'react-slick/lib/slider'
+
+const PrevButton = ({ currentSlide, slideCount, ...props }) => (
+    <>
+        <div className='home-brands-prev-btn' {...props}
+            style={{
+                backgroundImage: "linear-gradient(to left ,#F1CD6C, #f3dd9f)",
+                width: '5vw',
+                height: '5vw',
+                marginLeft: '-6vw',
+                color: '#121212',
+                backgroundColor: '#fff',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <i class="home-brands-prev-btn-icon bi bi-chevron-left"></i>
+        </div>
+    </>
+);
+
+const NextButton = ({ currentSlide, slideCount, ...props }) => (
+    <div className='home-brands-prev-btn' {...props}
+        style={{
+            backgroundImage: "linear-gradient(to left ,#F1CD6C, #f3dd9f)",
+            width: '5vw',
+            height: '5vw',
+            marginRight: '-6vw',
+            color: '#121212',
+            fontSize: '10vw',
+            backgroundColor: '#fff',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}
+    >
+        <i class="home-brands-prev-btn-icon bi bi-chevron-right"></i>
+    </div>
+);
 
 function HomeBrands() {
 
     const settings = {
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1500,
         dots: true,
         infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        nextArrow: <NextButton />,
+        prevArrow: <PrevButton />
     };
 
     return (
         <div className="home-brands-main">
-            {/* <div className="home-brands">
-                <div className="home-brands-header">
-                    <p className="home-brands-header-title">
-                        Brands
-                    </p>
-                </div>
-                <div className="home-brands-content">
-
-                    <div className="home-brands-content-btn  home-brands-content-btn-minus"
-                        style={{
-                            backgroundImage: "linear-gradient(to left ,#F1CD6C, #f3dd9f)",
-                            color: "darkred",
-                        }}
-
-                    >
-                        <i className="home-brands-content-btn-chevron bi bi-chevron-left"></i>
+            <div className="home-brands">
+                <Slider {...settings} className="home-brands-slider"
+                    style={{
+                        backgroundImage: "linear-gradient(to left ,#F1CD6C, #f3dd9f)",
+                    }}
+                >
+                    <div className='home-brands-slider-item'>
+                        <img src={Akko} alt="" className="home-brands-logo"
+                            style={{
+                                display: 'inline',
+                            }}
+                        />
                     </div>
-
-                    <div className="home-content-brands">
-                        <Slider {...settings}>
-                            <div>
-                                <h3>1</h3>
-                            </div>
-                            <div>
-                                <h3>2</h3>
-                            </div>
-                            <div>
-                                <h3>3</h3>
-                            </div>
-                            <div>
-                                <h3>4</h3>
-                            </div>
-                            <div>
-                                <h3>5</h3>
-                            </div>
-                            <div>
-                                <h3>6</h3>
-                            </div>
-                        </Slider>
+                    <div className='home-brands-slider-item'>
+                        <img src={Acer} alt="" className="home-brands-logo"
+                            style={{
+                                display: 'inline',
+                            }}
+                        />
                     </div>
-
-                    <div className="home-brands-content-btn  home-brands-content-btn-plus"
-                        style={{
-                            backgroundImage: "linear-gradient(to left ,#F1CD6C, #f3dd9f)",
-                            color: "darkred",
-                        }}
-                    >
-                        <i className="home-brands-content-btn-chevron bi bi-chevron-right"></i>
+                    <div className='home-brands-slider-item'>
+                        <img src={Amd} alt="" className="home-brands-logo"
+                            style={{
+                                display: 'inline',
+                            }}
+                        />
                     </div>
-                </div>
-            </div> */}
-            <Slider {...settings}>
-                <div>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
-                </div>
-                <div>
-                    <h3>6</h3>
-                </div>
-            </Slider>
+                    <div className='home-brands-slider-item'>
+                        <img src={AsRock} alt="" className="home-brands-logo"
+                            style={{
+                                display: 'inline',
+                            }}
+                        />
+                    </div>
+                    <div className='home-brands-slider-item'>
+                        <img src={Asus} alt="" className="home-brands-logo"
+                            style={{
+                                display: 'inline',
+                            }}
+                        />
+                    </div>
+                    <div className='home-brands-slider-item'>
+                        <img src={Corsair} alt="" className="home-brands-logo"
+                            style={{
+                                display: 'inline',
+                            }}
+                        />
+                    </div>
+                    <div className='home-brands-slider-item'>
+                        <img src={Dareu} alt="" className="home-brands-logo"
+                            style={{
+                                display: 'inline',
+                            }}
+                        />
+                    </div>
+                    <div className='home-brands-slider-item'>
+                        <img src={DeepCool} alt="" className="home-brands-logo"
+                            style={{
+                                display: 'inline',
+                            }}
+                        />
+                    </div>
+                    <div className='home-brands-slider-item'>
+                        <img src={Dell} alt="" className="home-brands-logo"
+                            style={{
+                                display: 'inline',
+                            }}
+                        />
+                    </div>
+                    <div className='home-brands-slider-item'>
+                        <img src={Hp} alt="" className="home-brands-logo"
+                            style={{
+                                display: 'inline',
+                            }}
+                        />
+                    </div>
+                    <div className='home-brands-slider-item'>
+                        <img src={Gs} alt="" className="home-brands-logo"
+                            style={{
+                                display: 'inline',
+                            }}
+                        />
+                    </div>
+                    <div className='home-brands-slider-item'>
+                        <img src={Tnc} alt="" className="home-brands-logo"
+                            style={{
+                                display: 'inline',
+                            }}
+                        />
+                    </div>
+                </Slider>
+            </div>
         </div>
     )
 }
