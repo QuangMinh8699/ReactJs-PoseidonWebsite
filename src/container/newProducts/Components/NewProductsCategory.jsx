2959b31afbf55dd863d3ddb2f7cc0529a68dddd5
Minh <quangminh8699@gmail.com>
2022-03-27 18:10:07 +0700

    update category new products page

--- src/container/newProducts/Components/NewProductsCategory.jsx
+++ src/container/newProducts/Components/NewProductsCategory.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import '../Assets/Css/NewProductsCategory.css'
 
 function NewProductsCategory() {
+
+    const [translateY, setTranslateY] = useState('');
+    const [rotate, setRotate] = useState('');
+
+    const handleChangeDisplay = () => {
+        setTranslateY('translateY(0vw)')
+        setRotate('rotate(90deg)')
+
+        if (translateY == 'translateY(0vw)' && rotate == 'rotate(90deg)') {
+            setTranslateY('translateY(-23vw)')
+            setRotate('none')
+        }
+
+
+    }
+
     return (
         <div className="new-products-category-main">
             <div className="new-products-category">
@@ -9,23 +26,35 @@ function NewProductsCategory() {
                     style={{
                         backgroundImage: "linear-gradient(to left ,#F1CD6C, #f3dd9f)",
                     }}
+
+                    onClick={handleChangeDisplay}
                 >
                     <p className="new-products-category-header-p">
                         CATEGORY
                     </p>
-                    <i class="new-products-category-header-icon bi bi-chevron-right"></i>
+                    <i class="new-products-category-header-icon bi bi-chevron-right"
+                        style={{
+                            transform: rotate,
+                            transition: '0.25s',
+                        }}
+                    ></i>
                 </div>
 
                 <div className="new-products-category-content">
-                    <ul className="new-products-category-ul">
-                        <li className="new-products-category-li">GAMING WORKSTATION</li>
-                        <li className="new-products-category-li">GAMING LAPTOP</li>
-                        <li className="new-products-category-li">PARTS OF COMPUTER</li>
-                        <li className="new-products-category-li">GAMING GEAR</li>
-                        <li className="new-products-category-li">COMPUTER SCREEN</li>
-                        <li className="new-products-category-li">GAMING CHAR</li>
-                        <li className="new-products-category-li">COOLING ACCESSORIES</li>
-                        <li className="new-products-category-li">NETWORK EQUIPMENT</li>
+                    <ul className="new-products-category-ul"
+                        style={{
+                            transform: translateY,
+                            transition: '0.25s',
+                        }}
+                    >
+                        <Link className="new-products-category-li" to='/new'><li >GAMING WORKSTATION</li></Link>
+                        <Link className="new-products-category-li" to='/new'><li >GAMING LAPTOP</li></Link>
+                        <Link className="new-products-category-li" to='/new'><li >PARTS OF COMPUTER</li></Link>
+                        <Link className="new-products-category-li" to='/new'><li >GAMING GEAR</li></Link>
+                        <Link className="new-products-category-li" to='/new'><li >COMPUTER SCREEN</li></Link>
+                        <Link className="new-products-category-li" to='/new'><li >GAMING CHAR</li></Link>
+                        <Link className="new-products-category-li" to='/new'><li >COOLING ACCESSORIES</li></Link>
+                        <Link className="new-products-category-li" to='/new'><li >NETWORK EQUIPMENT</li></Link>
                     </ul>
                 </div>
             </div>
